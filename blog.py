d08c96b00a43076124eced87b85a361165e56420
Svitlana Ripa <svitlanka.ripa@gmail.com>
2022-06-09 14:50:45 +0200

    removed pycache

--- blog.py
+++ blog.py
@@ -1,4 +1,10 @@
-from app import app
+from app import app, db
+from app.models import User, Post
+
+@app.shell_context_processors
+def make_shell_context():
+    return {'db':db, 'User':User, 'Post':Post}
+
 
 # Remember the two app entities? Here you can see both together in the same sentence.
 # The Flask application instance is called app and is a member of the app package.
